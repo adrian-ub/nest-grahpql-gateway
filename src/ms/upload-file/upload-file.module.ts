f095e10e508252da2991a7c9c8a0a60e6e9c1bbc
Adrián UB <22903142+adrian-ub@users.noreply.github.com>
2023-03-17 11:21:27 -0500

    feat: add integration ms upload

--- src/ms/upload-file/upload-file.module.ts
+++ src/ms/upload-file/upload-file.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UploadFileService } from './upload-file.service';
 import { UploadFileResolver } from './upload-file.resolver';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   providers: [UploadFileResolver, UploadFileService],
 })
 export class UploadFileModule {}
--- src/ms/upload-file/upload-file.service.ts
+++ src/ms/upload-file/upload-file.service.ts
@@ -1,13 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { createWriteStream, createReadStream as FsCreateReadStream } from 'fs';
+import { join } from 'path';
 import { CreateUploadFileInput } from './dto/create-upload-file.input';
+import * as FormData from 'form-data';
+import { firstValueFrom } from 'rxjs';
+import { endpoints } from 'src/config';
+import { HttpService } from '@nestjs/axios';
 
 @Injectable()
 export class UploadFileService {
-  create(createUploadFileInput: CreateUploadFileInput) {
-    return { file: true };
-  }
+  constructor(private readonly httpService: HttpService) {}
 
-  findAll() {
-    return [];
+  async create({ file }: CreateUploadFileInput) {
+    const { createReadStream, filename } = await file;
+
+    return new Promise(async (resolve) => {
+      createReadStream()
+        .pipe(createWriteStream(join(process.cwd(), 'src', 'upload', filename)))
+        .on('finish', async () => {
+          const form = new FormData();
+          form.append(
+            'myImage',
+            FsCreateReadStream(join(process.cwd(), 'src', 'upload', filename)),
+          );
+
+          console.log(form);
+
+          const { data } = await firstValueFrom(
+            this.httpService.post(`${endpoints.msUpload}/uploadphoto`, form, {
+              headers: form.getHeaders(),
+            }),
+          );
+
+          resolve({
+            file: data,
+          });
+        });
+    });
   }
 }
